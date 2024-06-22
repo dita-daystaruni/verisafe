649601632ae93a1c9fd054018968a7881edf8ac5
Erick <hearteric57@gmail.com>
2024-06-22 11:24:00 +0300

    fix: storing and retrieving user rewards based on ID

--- models/db/reward_transaction.go
+++ models/db/reward_transaction.go
@@ -32,7 +32,7 @@ func (rts *RewardTransactionStore) NewRewardTransaction(rewardTransaction models
 
 	// Add the points
 	student.VibePoints += rewardTransaction.Points
-	if err := rts.DB.Save(&student).Error; err != nil {
+	if err := rts.DB.Model(&student).Update("vibe_points", student.VibePoints).Error; err != nil {
 		return nil, err
 	}
 
