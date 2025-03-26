921e4e6fac0c01176c7f9e6a39af96ac0c371eaa
Erick <hearteric57@gmail.com>
2025-03-26 10:33:08 +0300

    chore: changed the ping message

--- internal/app/api.go
+++ internal/app/api.go
@@ -16,7 +16,7 @@ func RegisterHandlers(s *Server) {
 	s.Use(middlewares.LoggingMiddleware(logger))
 
 	s.GET("/ping", func(c *gin.Context) {
-		c.IndentedJSON(http.StatusOK, gin.H{"message": "its alive!"})
+		c.IndentedJSON(http.StatusOK, gin.H{"message": "i'm alive!"})
 	})
 
 	uh := handlers.UserHandler{Conn: s.Conn, Cfg: s.Config, Logger: logger}
